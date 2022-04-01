3b9d04fec17522c9b3537117caf419f891363407
Dedo-unuka <dzanazovko@gmail.com>
2022-04-01 10:37:54 +0200

    Adding ex06

--- ex06/src/index.js
+++ ex06/src/index.js
@@ -1,6 +1,7 @@
 function main() {
     // only change code below this line
-      
+      var myString = "Concatenation is the process of appending ";
+      myString += "one string to the end of another string.";
     // only change code above this line
     return myString;
 }    
