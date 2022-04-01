55aabef96c5e829251b58d0269e57f73bf3514e3
Dedo-unuka <dzanazovko@gmail.com>
2022-04-01 12:04:45 +0200

    Adding ex15

---
+++ ex15/src/index.js
@@ -0,0 +1,14 @@
+function main() {
+    // only change code below this line
+      var myNoun = "dog";
+      var myVerb = "ran";
+      var myAdjective = "big";
+      var myAdverb = "quickly";
+      var wordBlanks = "My " + myAdjective + " " + myNoun + " Mars " + myVerb + " very " + myAdverb + ".";
+      
+    // only change code above this line
+    return wordBlanks;
+}    
+
+console.log(main());
+module.exports = main;
