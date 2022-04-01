933a62ceb712ab12f9f0d464cc76e0178924bfda
Dedo-unuka <dzanazovko@gmail.com>
2022-04-01 10:32:18 +0200

    Adding ex05

--- ex06/src/index.js
+++ ex06/src/index.js
@@ -1,6 +1,6 @@
 function main() {
     // only change code below this line
-      var myString = "This is the first string. " + "This is the second string.";
+      
     // only change code above this line
     return myString;
 }    
