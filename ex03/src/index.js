8bf48990ee3ca83a5ed41028d063e4f8d6393aad
Dedo-unuka <dzanazovko@gmail.com>
2022-04-01 09:45:54 +0200

    Adding ex03

---
+++ ex03/src/index.js
@@ -0,0 +1,10 @@
+function main() {
+    // only change code below this line
+      var myLink = '<a href="https://www.example.com" target="_blank">Link</a>'
+
+    // only change code above this line
+    return myLink;
+}    
+
+console.log(main());
+module.exports = main;
