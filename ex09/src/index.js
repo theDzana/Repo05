0069cb0aef65d1234c38e07e279ea7dcc81665d0
Dedo-unuka <dzanazovko@gmail.com>
2022-04-01 11:15:38 +0200

    Adding ex09

---
+++ ex09/src/index.js
@@ -0,0 +1,10 @@
+function main() {
+    // only change code below this line
+      var lastName = "Doe";
+      lastNameLength = lastName.length;
+    // only change code above this line
+    return lastNameLength; 
+}    
+
+console.log(main());
+module.exports = main;
