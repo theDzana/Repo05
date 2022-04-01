f5706952384738740d6f1a8867c22364f5c2ebda
Dedo-unuka <dzanazovko@gmail.com>
2022-04-01 09:13:38 +0200

    Adding ex01

---
+++ ex01/src/index.js
@@ -0,0 +1,14 @@
+function main() {
+    // only change code below this line
+        var myFirstName = "Dzana";
+        var myLastName = "Zovko";
+        var myHomeTown = "Sarajevo";
+    // only change code above this line
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+}
+console.log(main());
+module.exports = main;
